28424711d60440c260fb253d846bc1369a497e4e
Alexander Obozny <alexander.obozny@gmail.com>
2015-08-02 19:49:08 +0300

    Implement form-based authentication

--- src/main/db/init.sql
+++ src/main/db/init.sql
@@ -1,8 +1,17 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS user_roles;
+
 CREATE TABLE users (
     id SERIAL,
-    name VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(20) NOT NULL,
-    roles VARCHAR(40) NOT NULL
+    user_name VARCHAR(20) PRIMARY KEY,
+    user_password VARCHAR(20) NOT NULL
 );
-INSERT INTO users (name, password, roles) VALUES ('admin', 'admin', 'admin,user');
+CREATE TABLE user_roles (
+    id SERIAL,
+    user_name varchar(20) NOT NULL,
+    role_name varchar(20) NOT NULL,
+    PRIMARY KEY (user_name, role_name)
+);
+
+INSERT INTO users (user_name, user_password) VALUES ('admin', 'admin');
+INSERT INTO user_roles (user_name, role_name) VALUES ('admin', 'admin');
