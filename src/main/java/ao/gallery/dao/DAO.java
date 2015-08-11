2be396ae3c818aefaa7a236f395f6c6310d5f11a
Alexander Obozny <alexander.obozny@gmail.com>
2015-08-11 23:40:40 +0300

    Implemented retrieving pictures for user from DB

--- src/main/java/ao/gallery/dao/DAO.java
+++ src/main/java/ao/gallery/dao/DAO.java
@@ -1,7 +1,11 @@
 package ao.gallery.dao;
 
+import java.util.List;
+
 public interface DAO {
 
     void addPicture(Picture picture) throws DAOException;
+
+    List<Picture> getUserPictures(String userName) throws DAOException;
 
 }
--- src/main/java/ao/gallery/dao/MySQLDAO.java
+++ src/main/java/ao/gallery/dao/MySQLDAO.java
@@ -1,9 +1,13 @@
 package ao.gallery.dao;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MySQLDAO implements DAO {
 
@@ -17,6 +21,7 @@ public class MySQLDAO implements DAO {
     private static final String USER_NAME = "alexander";
     private static final String USER_PASSWORD = "ao_password";
     private static final String INSERT_PICTURE_QUERY = "INSERT INTO users_pictures (picture_name, uploader_name, picture, thumbnail) VALUES (?, ?, ?, ?)";
+    private static final String SELECT_USER_PICTURES_QUERY = "SELECT * FROM users_pictures WHERE uploader_name = ?";
 
     public static MySQLDAO getInstance() {
         if (instance == null) {
@@ -45,6 +50,29 @@ public class MySQLDAO implements DAO {
         } catch (SQLException ex) {
             throw new DAOException(ex);
         }
+    }
+
+    @Override
+    public List<Picture> getUserPictures(String userName) throws DAOException {
+        List<Picture> pictures = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(DB_CONNECTION_URL, USER_NAME, USER_PASSWORD);
+                PreparedStatement statement = connection.prepareStatement(SELECT_USER_PICTURES_QUERY)) {
+            statement.setString(1, userName);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int pictureId = resultSet.getInt(1);
+                String pictureName = resultSet.getString(2);
+                // TODO do not query picture uploader name, it is parameter
+                String pictureUploaderName = resultSet.getString(3);
+                InputStream pictureContent = resultSet.getBinaryStream(4);
+                InputStream thumbnail = resultSet.getBinaryStream(5);
+                Picture picture = new Picture(pictureId, pictureName, pictureUploaderName, pictureContent, thumbnail);
+                pictures.add(picture);
+            }
+        } catch (SQLException ex) {
+            throw new DAOException(ex);
+        }
+        return pictures;
     }
 
 }
