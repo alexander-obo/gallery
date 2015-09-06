29ec53f76850ee7615a7dbb4265da85888af9f7e
Alexander Obozny <alexander.obozny@gmail.com>
2015-09-06 13:40:40 +0300

    To issues #5 and #6: validate English letters, numbers and special symbols in login

--- src/main/webapp/js/registration_validation.js
+++ src/main/webapp/js/registration_validation.js
@@ -1,7 +1,17 @@
+var LOGIN_DELIMITERS = [".", "-", "_"];
 $().ready(function () {
     $.validator.addMethod("noSpaces", function (value, element, param) {
         return value.indexOf(" ") === -1;
     }, "Spaces are now allowed");
+    $.validator.addMethod("loginValidation", function (value, element, param) {
+        for (i = 0; i < value.length - 1; i++) {
+            if (LOGIN_DELIMITERS.indexOf(value[i]) !== -1 && LOGIN_DELIMITERS.indexOf(value[i + 1]) !== -1) {
+                return false;
+            }
+        }
+        var regexp = new RegExp("^[A-Za-z\\d][A-Za-z\\d\\.\\-_]{1,18}[A-Za-z\\d]$");
+        return regexp.test(value);
+    }, "Login is not valid");
     $("#registration_form").validate({
         rules: {
             email: {
@@ -13,7 +23,8 @@ $().ready(function () {
                 required: true,
                 minlength: 3,
                 maxlength: 20,
-                noSpaces: true
+                noSpaces: true,
+                loginValidation: true
             },
             password: {
                 required: true,
